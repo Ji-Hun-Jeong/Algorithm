e2b059c2e6d494407dc9e5516601568ca9b9aa60
Ji-Hun-Jeong <wjdwlgns8096@naver.com>
2023-11-26 14:55:59 +0900

    K Sort

--- Algorithm/소스.cpp
+++ Algorithm/소스.cpp
@@ -1,69 +1,32 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
 using namespace std;
 
-class Graph
+int main()
 {
-private:
-	priority_queue<int, vector<int>, greater<int>> m_queueV[100001];
-	int m_iNumOfVertex;
-	bool m_arrVisited[100001];
-	int m_arrOrder[100001];
-	int m_iOrder;
-public:
-	Graph(int n)
-		: m_iNumOfVertex(n)
-		, m_iOrder(1)
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int arr[100];
+	srand(time(NULL));
+	for (int i = 0; i < 100; ++i)
+		arr[i] = rand() % 100;
+	for (int i = 0; i < 99; ++i)
 	{
-		for (int i = 0; i < 100001; ++i)
+		for (int j = 0; j < 100 - i; ++j)
 		{
-			m_arrVisited[i] = false;
-			m_arrOrder[i] = 0;
-		}
-	}
-public:
-	void insertEdge(int start, int end)
-	{
-		m_queueV[start].push(end);
-		m_queueV[end].push(start);
-	}
-	void dfs(int start)
-	{
-		m_arrOrder[start] = m_iOrder++;
-		m_arrVisited[start] = true;
-		while (!m_queueV[start].empty())
-		{
-			int i = m_queueV[start].top();
-			m_queueV[start].pop();
-			if (!m_arrVisited[i])
+			if (arr[j] > arr[j + 1])
 			{
-				dfs(i);
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
 	}
-	void showOrder()
-	{
-		for (int i = 1; i <= m_iNumOfVertex; ++i)
-		{
-			if (m_arrOrder[i])
-				printf("%d\n", m_arrOrder[i]);
-			else
-				printf("%d\n", 0);
-		}
-	}
-};
-int main()
-{
-	int n, m, r;
-	cin >> n >> m >> r;
-	Graph* myGraph = new Graph(n);
-	int u = 0, v = 0;
-	for (int i = 0; i < m; ++i)
-	{
-		scanf("%d %d", &u, &v);
-		myGraph->insertEdge(u, v);
-	}
-	myGraph->dfs(r);
-	myGraph->showOrder();
+	for (int i = 0; i < 100; ++i)
+		cout << arr[i] << "\n";
 }
